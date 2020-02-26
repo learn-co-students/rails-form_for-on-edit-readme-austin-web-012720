1717f1a09cba85c3686b554075da9a2b8dfb467e
lughjw <lughjw@gmail.com>
2020-02-25 19:06:07 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
@@ -16,6 +17,7 @@ class PostsController < ApplicationController
 	  @post.title = params[:title]
 	  @post.description = params[:description]
 	  @post.save
+
 	  redirect_to post_path(@post)
 	end
 
@@ -25,7 +27,9 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:title], description: params[:description])
+	  @post.update(params.require(:post).permit(:title, :description))
+	  @post.save
+
 	  redirect_to post_path(@post)
 	end
 end
